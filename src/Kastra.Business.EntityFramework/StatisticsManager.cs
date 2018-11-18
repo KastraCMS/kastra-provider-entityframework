e452dbea4951f2d6e10245e42bc5108663c0d283
Babat <babat42@gmail.com>
2018-11-18 23:01:49 +0100

    Prevent flood with the visit system

--- src/Kastra.Business.EntityFramework/StatisticsManager.cs
+++ src/Kastra.Business.EntityFramework/StatisticsManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Kastra.Business.Mappers;
 using Kastra.Core.Business;
 using Kastra.Core.DTO;
+using Kastra.Core.Services;
 using Kastra.DAL.EntityFramework;
 using Kastra.DAL.EntityFramework.Models;
 
@@ -11,15 +12,44 @@ namespace Kastra.Business
 {
     public class StatisticsManager : IStatisticsManager
     {
-        private readonly KastraContext _dbContext = null;
+        private const string VISITS_KEY = "visits_list";
 
-        public StatisticsManager(KastraContext dbContext)
+        #region Private members
+
+        private readonly KastraContext _dbContext = null;
+        private readonly CacheEngine _cacheEngine = null;
+
+        #endregion
+
+        public StatisticsManager(KastraContext dbContext, CacheEngine cacheEngine)
         {
             _dbContext = dbContext;
+            _cacheEngine = cacheEngine;
         }
 
         public bool SaveVisitor(VisitorInfo visitorInfo)
         {
+            Dictionary<string, DateTime> recentVisitors = null;
+
+            if (_cacheEngine.GetCacheObject<Dictionary<string, DateTime>>(VISITS_KEY, out recentVisitors))
+            {
+                if (recentVisitors.ContainsKey(visitorInfo.IpAddress))
+                {
+                    return false;
+                }
+                else 
+                {
+                    recentVisitors.Add(visitorInfo.IpAddress, DateTime.UtcNow);
+                }
+            }
+            else
+            {
+                recentVisitors = new Dictionary<string, DateTime>();
+                recentVisitors.Add(visitorInfo.IpAddress, DateTime.UtcNow);
+                _cacheEngine.SetCacheObject<Dictionary<string, DateTime>>(VISITS_KEY, recentVisitors);
+            }
+
+            // Save visit in database
             KastraVisitors visitor = visitorInfo.ToKastraVisitor();
 
             _dbContext.KastraVisitors.Add(visitor);
